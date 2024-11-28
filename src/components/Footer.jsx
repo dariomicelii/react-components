7a6d6f66e92a532df230495361f9b26ad3ffc973
dariomicelii <dariomiceli01@gmail.com>
2024-11-28 17:35:46 +0100

    Footer attuale

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <footer className="footer">
-      <div className="d-flex text-bg-secondary p-50">
+      <div className="d-flex text-bg-secondary">
         <div className="p-5">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis in
           placeat optio deserunt? Cupiditate vel voluptatum soluta repellendus
